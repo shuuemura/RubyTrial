14266969ce8e0b75deed82150efab11ccd9a58fb
shuuemura <suemura@netprotections.co.jp>
2021-01-21 20:29:15 +0900

    alter variable name

--- app/apis/api/ver1/cards.rb
+++ app/apis/api/ver1/cards.rb
@@ -8,39 +8,36 @@ module API
       end
 
       post '/ver1/cards' do
-        result = []
-        error = []
-        strength = []
+        results = []
+        errors = []
+        strengths = []
         params[:cards_list].each do |cards|
-          card_validation_service = CardValidationService.new.execute(cards)
+          validation_msg = CardValidationService.new.execute(cards)
           hand_judge_service = HandJudgeService.new
           hand_judge_service.execute(cards)
-          if card_validation_service.present?
-            error_element = {
+          if validation_msg.present?
+            error = {
               "cards": cards,
-              "msg": card_validation_service
+              "msg": validation_msg
             }
-            error.push error_element
+            errors.push error
           else
-            result_element = {
+            result = {
               "cards": cards,
               "hand": hand_judge_service.hand,
               "best": false
             }
-            strength.push hand_judge_service.strength
-            result.push result_element
+            strengths.push hand_judge_service.strength
+            results.push result
           end
         end
-        max_list = strength.each_index.select{|i|strength[i]==strength.max}
-        max_list.each do |i|
-          result[i][:best] = true
+        strongest_indexes = strengths.each_index.select{|i|strengths[i]==strengths.max}
+        strongest_indexes.each do |i|
+          results[i][:best] = true
         end
-        response = {
-          "result": result,
-          "error": error
-        }
-        response.delete(:result) if result.empty?
-        response.delete(:error) if error.empty?
+        response = {}
+        response[:result] = results if results.any?
+        response[:error] = errors if errors.any?
         return response
       end
 
--- spec/api/ver1/cards_spec.rb
+++ spec/api/ver1/cards_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 RSpec.describe "Cards", type: :request do
-  context "正常なデータをRequestした時" do
+  context "正常なデータをRequestした時（もっとも役の強い手札がひとつ）" do
     let(:params){{"cards_list": ["H1 H13 H12 H11 H10","H9 C9 S9 H2 C2","C13 D12 C11 H8 H7"]}}
-    it "カードと役、最も役の強い強いカードにはbest判定でtrueが返される" do
+    it "カードと役、最も役の強いカードにはbest判定でtrueが返される" do
       post "/api/ver1/cards", params: params
       response_body = JSON.parse(response.body)
 
@@ -15,6 +15,26 @@ RSpec.describe "Cards", type: :request do
       expect(response_body['result'][1]['cards']).to eq "H9 C9 S9 H2 C2"
       expect(response_body['result'][1]['hand']).to eq "フルハウス"
       expect(response_body['result'][1]['best']).to eq false
+      expect(response_body['result'][2]['cards']).to eq "C13 D12 C11 H8 H7"
+      expect(response_body['result'][2]['hand']).to eq "ハイカード"
+      expect(response_body['result'][2]['best']).to eq false
+    end
+  end
+  context "正常なデータをRequestした時（もっとも役の強い手札が複数）" do
+    let(:params){{"cards_list": ["H1 H13 H12 H11 H10","H9 H8 H7 H6 H5","C13 D12 C11 H8 H7"]}}
+    it "カードと役、最も役の強い強いカードにはbest判定でtrueが返される" do
+      post "/api/ver1/cards", params: params
+      response_body = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(response).to have_http_status "201"
+
+      expect(response_body['result'][0]['cards']).to eq "H1 H13 H12 H11 H10"
+      expect(response_body['result'][0]['hand']).to eq "ストレートフラッシュ"
+      expect(response_body['result'][0]['best']).to eq true
+      expect(response_body['result'][1]['cards']).to eq "H9 H8 H7 H6 H5"
+      expect(response_body['result'][1]['hand']).to eq "ストレートフラッシュ"
+      expect(response_body['result'][1]['best']).to eq true
       expect(response_body['result'][2]['cards']).to eq "C13 D12 C11 H8 H7"
       expect(response_body['result'][2]['hand']).to eq "ハイカード"
       expect(response_body['result'][2]['best']).to eq false
